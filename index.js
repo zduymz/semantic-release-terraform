7ed2fdae1184ca9362b03a1f17fc466d418dbe50
Duy Mai <kenvin.md@gmail.com>
2024-10-10 12:34:57 -0500

    fix: update publish function

--- index.js
+++ index.js
@@ -5,29 +5,21 @@ import publishPackage from "./lib/publish.js";
 import verifyAuth from "./lib/verify-auth.js";
 import verifyConfig from "./lib/verify-config.js";
 
-// verifyConditions	Verify the presence of the HCP_TOKEN environment variable, and verify the authentication method is valid.
-// prepare:
-//  - check module exists in the registry, if not, create it
-// addChannel	: idk, we don't need it
-// publish:
-//  - create module version
-//  - compress (tarball) the module
-//  - publish the module to the registry
-
 let verified;
 
 // pluginConfig: { orgname: string, publish: bool, tarballDir: string }
 export async function verifyConditions(pluginConfig, context) {
-  
-  if (!pluginConfig.hasOwnProperty('publish')) {
+  if (!pluginConfig.hasOwnProperty("publish")) {
     pluginConfig.publish = true;
   }
 
   // i don't know what this does
   if (context.options.publish) {
     const publishPlugin =
-      castArray(context.options.publish).find((config) => config.path && config.path === "semantic-release-terraform") || {};
-      console.log(publishPlugin)
+      castArray(context.options.publish).find(
+        (config) => config.path && config.path === "semantic-release-terraform"
+      ) || {};
+    console.log(publishPlugin);
     pluginConfig.orgName = defaultTo(pluginConfig.orgName, publishPlugin.orgName);
     pluginConfig.publish = defaultTo(pluginConfig.publish, publishPlugin.publish);
     pluginConfig.tarballDir = defaultTo(pluginConfig.tarballDir, publishPlugin.tarballDir);
@@ -54,10 +46,10 @@ export async function verifyConditions(pluginConfig, context) {
 
 export async function publish(pluginConfig, context) {
   const errors = verified ? [] : verifyConfig(pluginConfig);
-
+  let pkg;
   try {
     // Reload package.json in case a previous external step updated it
-    const pkg = await getPkg(pluginConfig, context);
+    pkg = await getPkg(pluginConfig, context);
     if (!verified && pluginConfig.publish !== false) {
       await verifyAuth(context);
     }
